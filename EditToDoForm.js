35aace5a2aca7f5c8aa03828823cade3b26c644f
Khussett <Hussett@gmail.com>
2024-01-18 18:25:14 -0500

    Add files via upload

--- EditToDoForm.js
+++ EditToDoForm.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const EditToDoForm = () => {
+
+export const EditToDoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState("")
+    const handleSubmit = e => {
+    
+        e.preventDefault();
+
+        editTodo(value, task.id);
+
+        setValue("")
+
+
+
+    }
+    
   return (
-    <div>
-      
-    </div>
+  <form className="Todoform" onSubmit={handleSubmit}>
+    <input type="text" className="todo-input" value={value} placeholder="Update Task"
+    onChange={(e) => setValue(e.target.value)} />
+    <button type="submit" className='todo-btn'>Update Task
+ </button>
+    
+</form>
   )
 }
 
-export default EditToDoForm
+export default EditToDoForm;
