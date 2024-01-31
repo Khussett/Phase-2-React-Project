25ba01a4eb23a80fa9b8b5e9471c45f3428bc8f6
Khussett <Hussett@gmail.com>
2024-01-30 21:54:45 -0500

    Add files via upload

--- Home.js
+++ Home.js
@@ -4,9 +4,10 @@ import {Link} from 'react-router-dom';
 const Home = () => {
   return (
 <div style={{ backgroundColor: "aqua" }}>
-     <h5>Welcome to Staff Solutions. Our web app seeks to meet the needs of staffing and
-        management with ease no matter your company's size. 
-      </h5>
+     <p>Welcome to Staff Solutions Simplified. Our web app seeks to meet the needs of staffing and
+        management with ease no matter your company's size. As keeping track of company 
+        personnel may sometimes become hectic, Staff Solutions Simplified is here to save the day.</p> 
+      
       <Link to="/" className="btn btn-danger">Take Me To The App</Link>
 
     </div>
