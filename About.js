208de7b7329e976ce9ad0d6a2b1611e21eec3e18
Khussett <Hussett@gmail.com>
2024-02-13 16:51:47 -0500

    Update About.js

--- About.js
+++ About.js
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom';
 const About = () => {
   return (
     <div style={{ backgroundColor: "aqua" }} >
-      <p>Our mission at Staff Solutions Simplified is to revlutionize and improve 
-        the way in which company staffing needs are handled and met. Our web application 
+      <p>Our mission at Staff Solutions Simplified is to revolutionize and improve 
+        how company staffing needs are handled and met. Our web application 
         serves as a simple and effective way to create, update, and remove staff information 
         as needed. No matter a company's staffing size, we have you covered. For additional 
         information contact us at (123) 456-7890 or send an email to
